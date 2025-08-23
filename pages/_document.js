2f16a8eb5fecd59c25dd9dacf6c830d2801ceddc
ANSHM30 <anshm.cse22@sbjit.edu.in>
2025-08-23 23:30:46 +0530

    New Changes 'added about us and how it works page in about us folder'

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         const originalRenderPage = ctx.renderPage;
 
         try {
+            
             ctx.renderPage = () =>
                 originalRenderPage({
                     enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
